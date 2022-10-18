bafa7f5a202d79feadc03720529edae4607a3537
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-18 12:58:47 -0500

    Create preload.php

---
+++ preload.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of One Project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+require __DIR__ . '/vendor/autoload.php';
+
+use Framework\Autoload\Preloader;
+
+(new Preloader())->load();
